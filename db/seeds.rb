a22006c61ec9e1c17512a9a5fdfa3ed44df09765
Ritsuki Toshima <ricky0912t@gmail.com>
2023-09-23 17:43:54 +0900

    views

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,6 @@ url = 'http://tmdb.lewagon.com/movie/top_rated'
 response = JSON.parse(URI.open(url).read)
 
 response['results'].each do |movie_hash|
-  puts
-  p movie_hash
   # create an instance with the hash
   Movie.create!(
     poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
